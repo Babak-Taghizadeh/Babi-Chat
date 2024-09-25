db985c55fe2ea883453310e5123b596d6bf1046e
Babak Taghizadeh <babak.tz98@gmail.com>
2024-09-25 14:57:58 +0330

    sensored :D

--- src/dummy-data/chats..ts
+++ src/dummy-data/chats..ts
@@ -1,7 +1,7 @@
 export const chats = [
     {
       name: "Angele",
-      msg: "Hello Mother Fucker",
+      msg: "Hello There!!",
       pic: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       lastMsg: "today",
       newMsg: "2",
@@ -22,7 +22,7 @@ export const chats = [
     },
     {
       name: "Angele",
-      msg: "Hello Mother Fucker",
+      msg: "Hello There!!",
       pic: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       lastMsg: "today",
       newMsg: "2",
@@ -43,7 +43,7 @@ export const chats = [
     },
     {
       name: "Angele",
-      msg: "Hello Mother Fucker",
+      msg: "Hello There!!",
       pic: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       lastMsg: "today",
       newMsg: "2",
@@ -64,7 +64,7 @@ export const chats = [
     },
     {
       name: "Angele",
-      msg: "Hello Mother Fucker",
+      msg: "Hello There!!",
       pic: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       lastMsg: "today",
       newMsg: "2",
